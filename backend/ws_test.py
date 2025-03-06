fbb26152ea9158c96172567f31514ca27dce40c1
amingma <jiama19999@gmail.com>
2025-03-06 17:08:45 -0600

    multiple joins from same script

--- backend/ws_test.py
+++ backend/ws_test.py
@@ -2,17 +2,24 @@ import asyncio
 import websockets
 import json
 
-async def test_websocket():
-    async with websockets.connect("ws://127.0.0.1:8000/ws") as websocket:
-        message = input("Enter JSON data: ")
-        message = json.loads(message)
-        while message["done"] == "no":
-            await websocket.send(json.dumps(message))  
-            print(f"Sent: {message}")
-            response = await websocket.recv()  
-            print(f"Received: {json.loads(response)}")
+async def test_websocket(room_id):
+    async with websockets.connect(f"ws://127.0.0.1:8000/ws/{room_id}") as websocket:
+        while True:
             message = input("Enter JSON data: ")
-            message = json.loads(message)
+            try:
+                message = json.loads(message)
+            except:
+                break
+            await websocket.send(json.dumps(message))  
+            print(f"Room {room_id} sent: {message}")
+            response = await websocket.recv()  
+            print(f"Room {room_id} received: {json.loads(response)}")
 
 # Run the async function
-asyncio.run(test_websocket())
+async def main():
+    x = input("Enter room ID: ")
+    t1 = asyncio.create_task(test_websocket(x))
+    t2 = asyncio.create_task(test_websocket(x))
+    await asyncio.gather(t1, t2)
+
+asyncio.run(main())
