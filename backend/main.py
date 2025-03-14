ccf47eb87dd13072844d9d64313265e49ca1dce0
amingma <jiama19999@gmail.com>
2025-03-13 23:45:20 -0500

    delete print statements

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from functions import get_room_id
 from bigroom import BigRoom
+from models import JoinRoomRequest
 
 app = FastAPI()
 origins = [
@@ -18,6 +19,9 @@ app.add_middleware(
 
 room_ids = {}
 rooms = {}
+room_ids["mcI5j0Kw"] = 1
+rooms["mcI5j0Kw"] = BigRoom()
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -30,23 +34,19 @@ def create_room():
     return {"code": invite_code}
 
 @app.post("/join-room")
-def join_room(room_id: str):
-    if room_id not in room_ids:
+def join_room(request: JoinRoomRequest):
+    if request.room_id not in room_ids:
         raise HTTPException(status_code=400, detail="Room ID not found!")
-    return {"code": room_id}
+    return {"code": request.room_id}
 
 @app.websocket("/ws/{room_id}")
 async def websocket_endpoint(ws: WebSocket, room_id: str):
     await ws.accept()
     playerName = await ws.receive_text()
-    print(f'{playerName} has joined the lobby!')
     rooms[room_id].addPlayer(playerName)
-    print(rooms[room_id].numPlayers())
     try:
         while True:
             data = await ws.receive_json()
             await ws.send_json(data)
     except WebSocketDisconnect:
         rooms[room_id].removePlayer(playerName)
-        print(f'{playerName} has left the lobby!')
-        print(rooms[room_id].numPlayers())
