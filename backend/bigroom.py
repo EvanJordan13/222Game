67f62d3195db99b7e333c0e5c175bbafce253676
amingma <jiama19999@gmail.com>
2025-04-07 23:30:40 -0500

    more functions

--- backend/bigroom.py
+++ backend/bigroom.py
@@ -19,7 +19,11 @@ class BigRoom:
         return len(self.players)
     
     def updateState(self, a):
-        if a["action"] == "shuffle":
+        if a["action"] == "draw_card":
+            self.room = self.room.draw_card(a["args"]["hand_id"], a["args"]["deck_id"], a["args"]["n"], a["args"]["from_bottom"])
+        elif a["action"] == "split_deck":
+            self.room, new_deck_id = self.room.split_deck(a["args"]["deck_id"], a["args"]["n"], a["args"]["pos"])
+        elif a["action"] == "shuffle":
             self.room = self.room.shuffle(a["args"]["deck_id"])
         elif a["action"] == "nothing":
             pass
@@ -40,6 +44,11 @@ class BigRoom:
             self.room = self.room.add_top(a["args"]["deck_id"], card)
         elif a["action"] == "flip_deck_card":
             self.room = self.room.flip_deck_card(a["args"]["deck_id"], a["args"]["idx"], a["args"]["face_up"])
+        elif a["action"] == "flip_deck":
+            self.room = self.room.flip_deck(a["args"]["deck_id"])
+        elif a["action"] == "move_deck":
+            # tuple?
+            pass
         elif a["action"] == "deck_peek":
             # discuss at meeting
             pass
